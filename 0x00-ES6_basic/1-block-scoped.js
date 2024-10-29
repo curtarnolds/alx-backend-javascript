e2121670f84e37d88cedc1263a6019085530b074
curtarnolds <afayedormawuli@gmail.com>
2024-10-29 14:05:19 +0000

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task;
+    var task2;
   }
 
   return [task, task2];
